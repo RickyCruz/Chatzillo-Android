430961d683113096d047c59d03a6a5255bfc5c3e
Ricky Cruz <rczdev@gmail.com>
2020-07-21 15:29:57 -0500

    Add Logout

--- app/src/main/java/com/konztic/chatzillo/activities/MainActivity.kt
+++ app/src/main/java/com/konztic/chatzillo/activities/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.konztic.chatzillo.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.viewpager.widget.ViewPager
+import com.google.firebase.auth.FirebaseAuth
 import com.konztic.chatzillo.R
 import com.konztic.chatzillo.adapters.PagerAdapter
 import com.konztic.chatzillo.common.ToolbarActivity
 import com.konztic.chatzillo.fragments.ChatFragment
 import com.konztic.chatzillo.fragments.InfoFragment
 import com.konztic.chatzillo.fragments.RatesFragment
+import com.konztic.chatzillo.utilities.goToActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : ToolbarActivity() {
@@ -24,6 +28,26 @@ class MainActivity : ToolbarActivity() {
 
         setUpViewPager(getPagerAdapter())
         setUpBottomNavigationBar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.general_options_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_log_out -> {
+                FirebaseAuth.getInstance().signOut()
+
+                goToActivity<LoginActivity> {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                }
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun getPagerAdapter(): PagerAdapter {
